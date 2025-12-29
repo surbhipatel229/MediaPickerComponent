0b76d4785bc10ad3cbaab79cb878efd6e4f73857
surbhi-iroid <surbhi.iroidsolutions@gmail.com>
2025-12-29 19:04:38 +0530

    updated

--- app/src/main/java/com/example/mediapicker/MainActivity.kt
+++ app/src/main/java/com/example/mediapicker/MainActivity.kt
@@ -5,10 +5,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
